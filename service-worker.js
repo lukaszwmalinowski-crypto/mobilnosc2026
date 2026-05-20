a35e336ab5d04c48f18ce970209e0d9fc68e4dd0
lukaszwmalinowski-crypto <lukasz.w.malinowski@gmail.com>
2026-05-21 01:10:50 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='edukacja-bez-granic-v10';
+const CACHE='edukacja-bez-granic-v12';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/logo.png?v=4','./assets/icon-192.png?v=10','./assets/icon-512.png?v=10'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
